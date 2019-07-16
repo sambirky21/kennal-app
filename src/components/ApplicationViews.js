976a13018e4485552ac06a678df8703a8f30e2b3
Samuel Birky <sbirky21@gmail.com>
2019-07-16 16:09:27 -0500

    created main module, fetching all and deleting animals

--- src/components/ApplicationViews.js
+++ src/components/ApplicationViews.js
@@ -4,6 +4,7 @@ import LocationList from './Location/LocationList'
 import EmployeeList from './Employee/EmployeeList'
 import AnimalList from './Animal/AnimalList'
 import OwnerList from './Owner/OwnerList'
+import APIManager from '../APIManager';
 
 export default class ApplicationViews extends Component {
 
@@ -17,33 +18,58 @@ export default class ApplicationViews extends Component {
     componentDidMount() {
         const newState = {}
 
-        fetch("http://localhost:5002/animals")
-            .then(r => r.json())
-            .then(animals => newState.animals = animals)
-            .then(() => fetch("http://localhost:5002/employees")
-            .then(r => r.json()))
-            .then(employees => newState.employees = employees)
-            .then(() => fetch("http://localhost:5002/owners")
-            .then (r => r.json()))
-            .then(owners => newState.owners = owners)
-            .then(() => fetch("http://localhost:5002/locations")
-            .then (r => r.json()))
-            .then(locations => newState.locations = locations)
-            .then(() => this.setState(newState))
+        APIManager.getAll("animals").then(animals => newState.animals = animals)
+        APIManager.getAll("employees").then(employees => newState.employees = employees)
+        APIManager.getAll("owners").then(owners => newState.owners = owners)
+        APIManager.getAll("locations").then(locations => newState.locations = locations)
+        .then(() => this.setState(newState))
     }
 
-        deleteAnimal = id => {
-            return fetch(`http://localhost:5002/animals/${id}`, {
-                method: "DELETE"
-            })
-            .then(e => e.json())
-            .then(() => fetch(`http://localhost:5002/animals`))
-            .then(e => e.json())
-            .then(animals => this.setState({
+    //     fetch("http://localhost:5002/animals")
+    //         .then(r => r.json())
+    //         .then(animals => newState.animals = animals)
+    //         .then(() => fetch("http://localhost:5002/employees")
+    //         .then(r => r.json()))
+    //         .then(employees => newState.employees = employees)
+    //         .then(() => fetch("http://localhost:5002/owners")
+    //         .then (r => r.json()))
+    //         .then(owners => newState.owners = owners)
+    //         .then(() => fetch("http://localhost:5002/locations")
+    //         .then (r => r.json()))
+    //         .then(locations => newState.locations = locations)
+    //         .then(() => this.setState(newState))
+
+    //     deleteAnimal = id => {
+    //         return fetch(`http://localhost:5002/animals/${id}`, {
+    //             method: "DELETE"
+    //         })
+    //         .then(e => e.json())
+    //         .then(() => fetch(`http://localhost:5002/animals`))
+    //         .then(e => e.json())
+    //         .then(animals => this.setState({
+    //             animals: animals
+    //         })
+    //     )
+    // }
+
+        deleteAnimal = (id) => {
+            return APIManager.removeAndList("animals", id)
+            .then((animals) => this.setState({
                 animals: animals
             })
         )
     }
+
+//     deleteAnimal = (id) => {
+//         const newState = {}
+//         return APIManager.removeAndList("animals", id)
+//         .then((animalList) => {
+//             console.log(animalList)
+//             newState.animals = animalList
+//             this.setState(newState)
+//         }
+//     )
+// }
 
         deleteEmployee = id => {
             return fetch(`http://localhost:5002/employees/${id}`, {
